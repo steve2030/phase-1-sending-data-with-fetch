57efccd5c18ed0078db82e64930b63bdc8f830e5
steve <stevemighty2030@gmail.com>
2022-08-25 17:32:32 +0300

    the assignment was completed successfully

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Add your code here
+function submitData(name, email) {
+    const userObject = {
+        name: name,
+        email: email
+    };
+    return fetch('http://localhost:3000/users', {
+            method: 'POST',
+            headers: {
+                "content-type": "application/json",
+                'accept': "application/json"
+            },
+
+            body: JSON.stringify(userObject)
+        })
+        .then(res => res.json())
+        .then(user => {
+            const userid = document.createElement('h1');
+            const body = document.querySelector('body');
+            userid.textContent = user.id;
+            body.appendChild(userid);
+
+        })
+        .catch(error => {
+            appendError(error);
+        });
+}
+
+function appendError(error) {
+    const divError = document.querySelector('.error');
+    divError.style.color = 'red';
+    divError.textContent = error.message
+
+}
